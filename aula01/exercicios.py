bb76326f414ea9b55a825245fbfcf8e09c30f003
Gustavo Lourenço da Silva <gustavolsilva@outlook.com>
2025-05-17 17:36:20 -0300

    feat: add exercise file

---
+++ aula01/exercicios.py
@@ -0,0 +1,10 @@
+# Crie um programa que o usário digita o seu nome e retorna o número de caracteres.
+#print(len(input('Digite seu nome: ')))
+
+# Crie um programa onde o usuário digite dois valores e apareça a soma.
+# print(int(input('Digite o primeiro valor: ')) + int(input('Digite o segundo valor: ')))
+
+# Refatore o Exercício 2 atribuindo variáveis
+nome = input('Digite seu nome: ')
+qtd_caracteres = len(nome)
+print(qtd_caracteres)
