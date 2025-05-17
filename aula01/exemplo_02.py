f1ca735c1b9abccf0b7bb8c9b208b7a7279a9a20
Gustavo Lourenço da Silva <gustavolsilva@outlook.com>
2025-05-17 17:35:39 -0300

    feat: add second file - exemplo_02.py

---
+++ aula01/exemplo_02.py
@@ -0,0 +1,21 @@
+# Criar um programa onde o usuario digita dois valores e apareça a soma.
+
+print(int(input('Digite o primeiro valor: ')) + int(input('Digite o segundo valor: ')))
+
+numero = 3 # int
+numero_decimal = 4.78 # float
+nome_de_usuario = "Gustavo" # str
+verdadeiro = True # bool (True ou False)
+variavel = 4
+VARIaVEL = 4
+
+# Para ver o tipo de dado de uma variável, use o comando type()
+print(type(numero)) # int
+print(type(numero_decimal)) # float
+print(type(nome_de_usuario)) # str
+print(type(verdadeiro)) # bool
+
+
+
+
+
