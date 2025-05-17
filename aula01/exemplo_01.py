ee7f892d181af32123533cbf380a95e35341abe9
Gustavo Lourenço da Silva <gustavolsilva@outlook.com>
2025-05-17 17:40:20 -0300

    refactor: modified content into exemplo_01.py

--- aula01/exemplo_01.py
+++ aula01/exemplo_01.py
@@ -1,1 +1,15 @@
-print("second File")
+# Exemplo de impressão em tela de String
+print("Alguma coisa")
+
+# Exemplo de Impressão de tela de uma soma
+print(3 + 4) 
+
+# Exemplo de impressão em tela de uma concatenção
+print("Olá" + " " + "Turma")
+
+# Exemplo print do input
+print(input("Digite seu nome: "))
+
+# Exemplo de impressão de input concatenado
+print("Olá " + input("Digite seu nome: ") + "!")
+
